a9fa9496a7cb40876f5c04fb7e062d927b445c8d
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2025-05-16 07:51:38 -0300

    refactoring endpoint processamento

--- web_scrapers/vitibrasil_scraper.py
+++ web_scrapers/vitibrasil_scraper.py
@@ -16,9 +16,14 @@ class VitibrasilScraper:
     url_producao = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
 
     url_processamento_viniferas = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_01&opcao=opt_03'
+    #url_processamento_viniferas = 'file:///Users/mac/Desktop/dev/teste%202/processamento_viniferas.html'
     url_americanas_hibridas = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_02&opcao=opt_03'
+    # url_americanas_hibridas = 'file:///Users/mac/Desktop/dev/teste%202/processamento_americanas_hibridas.html'
     url_uvas_de_mesa = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_03&opcao=opt_03'
+    # url_uvas_de_mesa = 'file:///Users/mac/Desktop/dev/teste%202/processamento_uvas_de_mesa.html'
     url_sem_classificacao = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_04&opcao=opt_03'
+    #url_sem_classificacao = 'file:///Users/mac/Desktop/dev/teste%202/processamento_sem_classificacao.html'
+    
 
     def __init__(self):
         chrome_options = Options()
@@ -58,17 +63,7 @@ class VitibrasilScraper:
                     if match:
                         ano = match.group(1)
 
-                tabela = None
-                for t in soup.find_all("table"):
-                    primeira_linha = t.find("tr")
-                    if not primeira_linha:
-                        continue
-                    cabecalho_elementos = primeira_linha.find_all(["th", "td"])
-                    if cabecalho_elementos:
-                        cabecalho = [th.get_text(strip=True).upper() for th in cabecalho_elementos]
-                        if "CULTIVAR" in cabecalho or "SEM DEFINIÇÃO" in cabecalho:
-                            tabela = t
-                            break
+                tabela = soup.find("table", class_="tb_dados")
 
                 if tabela:
                     dados = []
@@ -89,18 +84,19 @@ class VitibrasilScraper:
                                 dados.append(item)
 
                     if not dados and categoria != 'sem_classificacao':
-                        raise ScrapingError(f"A tabela para a categoria '{categoria}' foi encontrada, mas não contém dados significativos. Possível erro de carregamento.")
+                        raise ScrapingError(f"A tabela com classe 'tb_dados' para a categoria '{categoria}' foi encontrada, mas não contém dados significativos. Possível erro de carregamento.")
 
                     return {"categoria": categoria, "dados": dados}
                 else:
-                    print(f"Tabela não encontrada na URL {url} na tentativa {attempt + 1}.")
+                    print(f"Tabela com classe 'tb_dados' não encontrada na URL {url} na tentativa {attempt + 1}.")
                     time.sleep(retry_delay)
 
             except Exception as e:
                 print(f"Erro ao processar URL {url} na tentativa {attempt + 1}: {e}")
                 time.sleep(retry_delay)
+                raise
 
-        raise ScrapingError(f"Falha ao encontrar a tabela após {max_retries} tentativas na URL: {url}")
+        raise ScrapingError(f"Falha ao encontrar a tabela com classe 'tb_dados' após {max_retries} tentativas na URL: {url}")
 
     def __del__(self):
         if self.driver:

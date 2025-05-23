707f58d9696af1e860d92fad98a16578f996e055
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2025-05-23 08:52:58 -0300

    update scraper

--- web_scrapers/vitibrasil_scraper.py
+++ web_scrapers/vitibrasil_scraper.py
@@ -64,6 +64,7 @@ class VitibrasilScraper:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--window-size=1920x1080')
         chrome_options.add_argument('--user-agent=Mozilla/5.0')
